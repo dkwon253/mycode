4f2761ca8f810b4423dccd7fcaccf3b6e427a1a0
David Kwon <dkwon23@gmail.com>
2021-03-29 17:48:57 +0000

    first push

---
+++ myveryfirstpythonscript.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# 1. Linux needs this to know how to run this file!
+# 2. Lets the reader know what kind of file this is.
+# 3. What is the significance of the .py?
+
+
+print("ello!")
+print("Did you say, hello?")
+print("No, I said ello, but that\'s close enough.")
